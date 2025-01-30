78b3121f2c9772ce73ca2f3e511a9dbbd71635c0
henrynoyes <107648313+henrynoyes@users.noreply.github.com>
2025-01-30 14:27:52 -0500

    modify build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j1
 
 cd ../../g2o
 
@@ -13,7 +13,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j4
 
 cd ../../Sophus
 
@@ -22,7 +22,20 @@ echo "Configuring and building Thirdparty/Sophus ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j4
+sudo make install
+
+echo "Configuring and building Pangolin ..."
+
+cd ../..
+
+git clone --recursive https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin
+./scripts/install_prerequisites.sh recommended
+cmake -B build
+cmake --build build
+cd build
+sudo make install
 
 cd ../../../
 
